e93e82457c56c16884810dc14e62c0a0d7eb245d
Jcorvil <jcorvil600@g.educaand.es>
2023-03-27 12:24:05 +0200

    UD7

--- src/Ejemplos/HolaMundo.java
+++ src/Ejemplos/HolaMundo.java
@@ -5,11 +5,50 @@ import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) throws IOException {
+        //Pedir al usuario que introduzca 3 caracteres por teclado
 
+        System.out.println("Introduzca tres(3) caracteres individuales: ");
+        int char1 = System.in.read();
+        int char2 = System.in.read();
+        int char3 = System.in.read();
+
+        //Vamos a imprimirlos concatenados
+        System.out.println("1.Usando 'write'");
+        System.out.write(char1);
+        System.out.write(char2);
+        System.out.write(char3);
+        System.out.flush();
+        System.out.println();
+        System.out.printf("\n •:•:•:•:•:•:•:•:•:•\n");
+        System.out.println("2.Usando 'print'");
+        System.out.print((char)char1 +""+ (char)char2 +""+ (char)char3);
+        System.out.printf("\n •:•:•:•:•:•:•:•:•:•\n");
+        System.out.println("3.Usando 'println':");
+        System.out.println((char)char1 +""+ (char)char2 +""+ (char)char3);
+        System.out.printf("\n •:•:•:•:•:•:•:•:•:•\n");
+
+
+        System.out.println("Usando scanner");
+        Scanner caracteres = new Scanner(System.in);
+        char char4 = caracteres.next().charAt(0);
+        char char5 = caracteres.next().charAt(0);
+        char char6 = caracteres.next().charAt(0);
+        System.out.println("1.Usando 'write'");
+        System.out.write(char4 + char5 + char6);
+        System.out.printf("\n •:•:•:•:•:•:•:•:•:•\n");
+        System.out.println("2.Usando 'print'");
+        System.out.print(char4 +""+ char5 +""+ char6);
+        System.out.printf("\n •:•:•:•:•:•:•:•:•:•\n");
+        System.out.println("3.Usando 'println':");
+        System.out.println(char4 +""+ char5 +""+ char6);
+
+
+        /*
         System.out.println("Introduzca su nombre: ");
         Scanner scannerInput = new Scanner(System.in);
         String nombre = scannerInput.nextLine();
         System.out.println(nombre);
+        */
 
         /*
         System.out.println("Introduzca su nombre de hasta 4 caracteres: ");
@@ -39,6 +78,7 @@ public class HolaMundo {
         System.out.flush();
         System.out.println("");
         */
+
         /*
         if(args.length > 0){
             System.out.println("Hola: " + args[0]);
---
+++ src/Ejercicios/EjercicioCalculadora.java
@@ -0,0 +1,8 @@
+package Ejercicios;
+
+public class EjercicioCalculadora {
+    public static void main(String[] args) {
+
+
+    }
+}
